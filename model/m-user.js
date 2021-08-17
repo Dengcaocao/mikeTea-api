b1e0b52270eb6a93aef18bb03db7784805a85681
邓威 <1536698087@qq.com>
2021-08-17 22:09:28 +0800

    创建user model

---
+++ model/m-user.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+const baseModel = require('./m-default')
+
+const user = mongoose.Schema({
+  telephone: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  nickname: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String,
+    default: null
+  },
+  // TODO 集合关联
+  member: {
+    type: String,
+    default: 4
+  },
+  // 描述
+  bio: {
+    type: String,
+    default: ''
+  },
+  baseModel
+})
+
+module.exports = user
